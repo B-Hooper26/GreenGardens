01b3a8603467e0bc34267927a84b3cde270b5b10
Benjamin Hooper <HOO19000635@btc.ac.uk>
2024-03-26 11:14:12 +0000

    Add project files.

---
+++ GreenGardens/Model/product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenGardens.Model
+{
+    public class product //Creating the table for each product
+    {
+        [Key]
+        public int Productid { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+        public int Stock_Quantity { get; set; }
+
+        public int Expected_Stock { get; set; }
+
+        public string ImagePath { get; set; }
+
+    }
+}
