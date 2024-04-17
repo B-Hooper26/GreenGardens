b4c96bd109a81753e2e9bdba06e1d974e6da7add
Benjamin Hooper <HOO19000635@btc.ac.uk>
2024-04-17 15:50:13 +0100

    Delete button has been made, however hasnt been tested.

---
+++ GreenGardens/Pages/Delete.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GreenGardens.Data;
+using GreenGardens.Model;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace GreenGardens.Pages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AppDbContext _dbConnection;
+
+        public product Item { get; set; }
+
+        public DeleteModel(AppDbContext context)
+        {
+            _dbConnection = context;
+        }
+
+        public void OnGet(Guid Productid)
+        {
+            // Retrieve the item to be deleted
+            Item = _dbConnection.product.FirstOrDefault(t => t.Productid == Productid);
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid Productid)
+        {
+            var itemToDelete = _dbConnection.product.FirstOrDefault(t => t.Productid == Productid);
+            if (itemToDelete != null)
+            {
+                _dbConnection.product.Remove(itemToDelete);
+                await _dbConnection.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                // Handle the case where the item does not exist
+                return NotFound();
+            }
+        }
+    }
+}
